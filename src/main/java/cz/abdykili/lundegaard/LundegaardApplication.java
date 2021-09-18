1644f9ae6e45aa75d7edef99312ac56d4a7580a5
Ilias Abdykarov <abdykili@abdykili.abdykili.com>
2021-09-18 10:05:12 +0200

    Init a project

---
+++ src/main/java/cz/abdykili/lundegaard/LundegaardApplication.java
@@ -0,0 +1,13 @@
+package cz.abdykili.lundegaard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LundegaardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LundegaardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/abdykili/lundegaard/LundegaardApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.abdykili.lundegaard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LundegaardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
